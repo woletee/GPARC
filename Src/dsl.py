fbd8466af7c982a49925323ad2461e818f22b88c
Liyew <118590747+woletee@users.noreply.github.com>
2025-01-06 21:02:11 +0900

    Update dsl.py

--- Src/dsl.py
+++ Src/dsl.py
@@ -1,1 +1,7 @@
-
+import numpy as np
+def rotate(input_grid):
+  return np.rot90(input_grid)
+def rotate180(input_grid):
+  return np.rot180(input_grid)
+def rotate270(input_grid):
+  retunr np.rot270(input_grid)
